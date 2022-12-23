d8fe9399db6c8dbfc15ca5aa142375723e631d58
c22dev <constclerc@gmail.com>
2022-12-23 01:13:14 +0100

    fixed airspam ui but created another UI problem

--- TrollBox/Tools/AirSpam/AirSpam.swift
+++ TrollBox/Tools/AirSpam/AirSpam.swift
@@ -23,14 +23,14 @@ struct AirSpam: View {
     var body: some View {
         NavigationView {
             Group {
-                if trollController.people.count == 0 { // No users in radius
                     VStack {
-                        ProgressView()
-                        Text("Searching for devices...")
-                            .foregroundColor(.secondary)
-                            .padding()
-                    }
-                } else {
+                            LazyVGrid(columns: gridItemLayout, spacing: 8) {
+                                ForEach(trollController.people.sorted(by: { a, b in a.displayName ?? "" < b.displayName ?? "" }), id: \.node) { p in
+                                    PersonView(person: p, selected: $selectedPeople[p.node])
+                                        .environmentObject(trollController)
+                                }
+                            }
+                        .padding()
                         VStack {
                             if trollController.isRunning { Text("Sent AirDrops: \(totalAirDrops)") }
                             HStack { // delay control between airdrops
@@ -53,92 +53,89 @@ struct AirSpam: View {
                         .padding()
                     }
                 }
-            }
-            .onAppear {
-                // Start searching nodes
-                trollController.startBrowser()
-            }
-            .onChange(of: rechargeDuration) { newValue in
-                trollController.rechargeDuration = newValue
-            }
-            
+        }
+        .navigationViewStyle(StackNavigationViewStyle())
+        .onAppear {
+            // Start searching nodes
+            trollController.startBrowser()
+        }
+        .onChange(of: rechargeDuration) { newValue in
+            trollController.rechargeDuration = newValue
         }
         
-        // shows a privacy req dialog if needed
-        func showPicker() {
-            PHPhotoLibrary.requestAuthorization(for: .readWrite) { status in
-                DispatchQueue.main.async {
-                    // show picker if authorized
-                    showingImagePicker = status == .authorized
-                }
-            }
-        }
-        
-        func toggleTrollButtonTapped() {
-            UIImpactFeedbackGenerator(style: .medium).impactOccurred() // mmm
-            
-            guard selectedPeople.values.filter({ $0 == true }).count > 0 else {
-                UIApplication.shared.alert(title: "No people selected", body: "Select users by tapping on them.")
-                return
-            }
-            
-            if !trollController.isRunning {
-                UIApplication.shared.confirmAlert(title: "\(UIDevice.current.name)", body: "This is the current name of this device and the name people will see when receiving an AirDrop. Are you sure you want to continue?", onOK: {
-                    if let imageURL = imageURL {
-                        trollController.sharedURL = imageURL
-                    }
-                    trollController.startTrolling(shouldTrollHandler: { person in
-                        return selectedPeople[person.node] ?? false // troll only selected people
-                    }, eventHandler: { event in
-                        switch event {
-                        case .operationEvent(let event1):
-                            if event1 == .canceled || event1 == .finished || event1 == .blocked {
-                                totalAirDrops += 1
-                                UISelectionFeedbackGenerator().selectionChanged()
-                            }
-                        case .cancelled:
-                            totalAirDrops += 1
-                            UISelectionFeedbackGenerator().selectionChanged()
-                        }
-                    }) // start trolling :troll:
-                    trollController.isRunning.toggle()
-                }, noCancel: false)
-            } else {
-                trollController.stopTrollings()
-                trollController.isRunning.toggle()
-            }
-        
-        struct PersonView: View {
-            @State var person: TrollController.Person
-            @Binding var selected: Bool?
-            @EnvironmentObject var trollController: TrollController
-            
-            var body: some View {
-                Button(action: {
-                    UIImpactFeedbackGenerator(style: .light).impactOccurred()
-                    if selected == nil { selected = false }
-                    selected?.toggle()
-                    remLog("selected", selected!)
-                }) {
-                    VStack {
-                        ZStack {
-                            Image((selected ?? false) ? "TrolledPerson" : "NonTrolledPerson")
-                        }
-                        Text(person.displayName ?? "Unknown")
-                            .font(.footnote)
-                            .multilineTextAlignment(.center)
-                            .minimumScaleFactor(0.5)
-                            .foregroundColor(.init(uiColor14: .label))
-                    }
-                }
-                .disabled(trollController.isRunning)
+    }
+    
+    // shows a privacy req dialog if needed
+    func showPicker() {
+        PHPhotoLibrary.requestAuthorization(for: .readWrite) { status in
+            DispatchQueue.main.async {
+                // show picker if authorized
+                showingImagePicker = status == .authorized
             }
         }
     }
+    
+    func toggleTrollButtonTapped() {
+        UIImpactFeedbackGenerator(style: .medium).impactOccurred() // mmm
+        
+        guard selectedPeople.values.filter({ $0 == true }).count > 0 else {
+            UIApplication.shared.alert(title: "No people selected", body: "Select users by tapping on them.")
+            return
+        }
+        
+        if !trollController.isRunning {
+            UIApplication.shared.confirmAlert(title: "\(UIDevice.current.name)", body: "This is the current name of this device and the name people will see when receiving an AirDrop. Are you sure you want to continue?", onOK: {
+                trollController.startTrolling(shouldTrollHandler: { person in
+                    return selectedPeople[person.node] ?? false // troll only selected people
+                }, eventHandler: { event in
+                    switch event {
+                    case .operationEvent(let event1):
+                        if event1 == .canceled || event1 == .finished || event1 == .blocked {
+                            totalAirDrops += 1
+                            UISelectionFeedbackGenerator().selectionChanged()
+                        }
+                    case .cancelled:
+                        totalAirDrops += 1
+                        UISelectionFeedbackGenerator().selectionChanged()
+                    }
+                }) // start trolling :troll:
+                trollController.isRunning.toggle()
+            }, noCancel: false)
+        } else {
+            trollController.stopTrollings()
+            trollController.isRunning.toggle()
+        }
+    }
+    
+    struct PersonView: View {
+        @State var person: TrollController.Person
+        @Binding var selected: Bool?
+        @EnvironmentObject var trollController: TrollController
+        
+        var body: some View {
+            Button(action: {
+                UIImpactFeedbackGenerator(style: .light).impactOccurred()
+                if selected == nil { selected = false }
+                selected?.toggle()
+                remLog("selected", selected!)
+            }) {
+                VStack {
+                    ZStack {
+                        Image((selected ?? false) ? "TrolledPerson" : "NonTrolledPerson")
+                    }
+                    Text(person.displayName ?? "Unknown")
+                        .font(.footnote)
+                        .multilineTextAlignment(.center)
+                        .minimumScaleFactor(0.5)
+                        .foregroundColor(.init(uiColor14: .label))
+                }
+            }
+            .disabled(trollController.isRunning)
+        }
+    }
 }
-    struct AirSpam_Previews: PreviewProvider {
+struct AirSpam_Previews: PreviewProvider {
         static var previews: some View {
             AirSpam()
         }
     }
-
