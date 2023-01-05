e139dff035e510a94d9933c06cbb29ad3c2e56cb
c22dev <constclerc@gmail.com>
2023-01-05 20:33:20 +0100

    oops

--- ipabuild.sh
+++ ipabuild.sh
@@ -8,7 +8,12 @@ WORKING_LOCATION="$(pwd)"
 APPLICATION_NAME=TrollBox
 CONFIGURATION=Debug
 
+if [ ! -d "build" ]; then
+    mkdir build
+fi
+
 cd build
+
 if [ -e "$APPLICATION_NAME.tipa" ]; then
 rm $APPLICATION_NAME.tipa
 fi
