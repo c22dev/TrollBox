e0e1f6c72f7fc84789bdfb76f42fefc248c94e72
c22dev <constclerc@gmail.com>
2023-01-18 00:24:14 +0100

    Custom audio importing beginning

---
+++ TrollBox/Tools/Sounds/AudioChangerView.swift
@@ -0,0 +1,241 @@
+////
+////  AudioChangerView.swift
+////  Cowabunga
+////
+////  Created by lemin on 1/9/23.
+////
+//
+//import SwiftUI
+//
+//struct AudioChangerView: View {
+//    var SoundIdentifier: AudioFiles.SoundEffect
+//    
+//    // included audio files
+//    struct IncludedAudioName: Identifiable {
+//        var id = UUID()
+//        var attachment: AudioFiles.SoundEffect
+//        var audioName: String
+//        var checked: Bool = false
+//    }
+//    
+//    // custom audio files
+//    struct CustomAudioName: Identifiable {
+//        var id = UUID()
+//        var audioName: String
+//        var displayName: String
+//        var checked: Bool = false
+//    }
+//    
+//    // list of included audio files
+//    @State var audioFiles: [IncludedAudioName] = [
+//        // charging
+//        .init(attachment: AudioFiles.SoundEffect.charging, audioName: "Default"),
+//        .init(attachment: AudioFiles.SoundEffect.charging, audioName: "Old"),
+//        .init(attachment: AudioFiles.SoundEffect.charging, audioName: "Engage"),
+//        .init(attachment: AudioFiles.SoundEffect.charging, audioName: "MagSafe"),
+//        .init(attachment: AudioFiles.SoundEffect.charging, audioName: "Cow"),
+//        
+//        // lock
+//        .init(attachment: AudioFiles.SoundEffect.lock, audioName: "Default"),
+//        .init(attachment: AudioFiles.SoundEffect.lock, audioName: "Old"),
+//        
+//        // notification
+//        .init(attachment: AudioFiles.SoundEffect.notification, audioName: "Default"),
+//        .init(attachment: AudioFiles.SoundEffect.notification, audioName: "Samsung"),
+//        .init(attachment: AudioFiles.SoundEffect.notification, audioName: "Taco Bell"),
+//        
+//        // screenshot
+//        .init(attachment: AudioFiles.SoundEffect.screenshot, audioName: "Default"),
+//        
+//        // sent message
+//        .init(attachment: AudioFiles.SoundEffect.sentMessage, audioName: "Default"),
+//        
+//        // received message
+//        .init(attachment: AudioFiles.SoundEffect.receivedMessage, audioName: "Default"),
+//        
+//        // payment success
+//        .init(attachment: AudioFiles.SoundEffect.paymentSuccess, audioName: "Default"),
+//    ]
+//    
+//    // list of custom audio files
+//    @State var customAudio: [CustomAudioName] = [
+//    ]
+//    
+//    // applied sound
+//    @State private var appliedSound: String = "Default"
+//    
+//    @State private var isImporting: Bool = false
+//    
+//    var body: some View {
+//        VStack {
+//            List {
+//                Section {
+//                    ForEach($audioFiles) { audio in
+//                        if audio.attachment.wrappedValue == SoundIdentifier {
+//                            // create button
+//                            HStack {
+//                                Image(systemName: "checkmark")
+//                                    .resizable()
+//                                    .aspectRatio(contentMode: .fit)
+//                                    .frame(width: 15, height: 15)
+//                                    .foregroundColor(.blue)
+//                                    .opacity(audio.checked.wrappedValue ? 1: 0)
+//                                
+//                                Button(audio.audioName.wrappedValue, action: {
+//                                    if appliedSound != audio.audioName.wrappedValue {
+//                                        for (i, file) in audioFiles.enumerated() {
+//                                            if file.audioName == appliedSound {
+//                                                audioFiles[i].checked = false
+//                                            } else if file.audioName == audio.audioName.wrappedValue {
+//                                                audioFiles[i].checked = true
+//                                            }
+//                                            
+//                                            for (i, file) in customAudio.enumerated() {
+//                                                if file.audioName == appliedSound {
+//                                                    customAudio[i].checked = false
+//                                                } else if file.audioName == audio.audioName.wrappedValue {
+//                                                    customAudio[i].checked = true
+//                                                }
+//                                            }
+//                                        }
+//                                        appliedSound = audio.audioName.wrappedValue
+//                                        // save to defaults
+//                                        UserDefaults.standard.set(appliedSound, forKey: SoundIdentifier.rawValue+"_Applied")
+//                                    }
+//                                })
+//                                .padding(.horizontal, 8)
+//                                .foregroundColor(.primary)
+//                            }
+//                        }
+//                    }
+//                } header: {
+//                    Text("Included")
+//                }
+//                
+//                Section {
+//                    ForEach($customAudio) { audio in
+//                        // create button
+//                        HStack {
+//                            Image(systemName: "checkmark")
+//                                .resizable()
+//                                .aspectRatio(contentMode: .fit)
+//                                .frame(width: 15, height: 15)
+//                                .foregroundColor(.blue)
+//                                .opacity(audio.checked.wrappedValue ? 1: 0)
+//                            
+//                            Button(audio.displayName.wrappedValue, action: {
+//                                if appliedSound != audio.audioName.wrappedValue {
+//                                    for (i, file) in audioFiles.enumerated() {
+//                                        if file.audioName == appliedSound {
+//                                            audioFiles[i].checked = false
+//                                        } else if file.audioName == audio.audioName.wrappedValue {
+//                                            audioFiles[i].checked = true
+//                                        }
+//                                    }
+//                                    
+//                                    for (i, file) in customAudio.enumerated() {
+//                                        if file.audioName == appliedSound {
+//                                            customAudio[i].checked = false
+//                                        } else if file.audioName == audio.audioName.wrappedValue {
+//                                            customAudio[i].checked = true
+//                                        }
+//                                    }
+//                                    appliedSound = audio.audioName.wrappedValue
+//                                    // save to defaults
+//                                    UserDefaults.standard.set(appliedSound, forKey: SoundIdentifier.rawValue+"_Applied")
+//                                }
+//                            })
+//                            .padding(.horizontal, 8)
+//                            .foregroundColor(.primary)
+//                        }
+//                    }
+//                } header: {
+//                    Text("Custom")
+//                }
+//            }
+//        }
+//        .navigationTitle(SoundIdentifier.rawValue)
+////        .toolbar {
+////            Button(action: {
+////                // import a custom audio
+////                // allow the user to choose the file
+////                isImporting = true
+////            }, label: {
+////                Image(systemName: "square.and.arrow.down")
+////            })
+////        }
+//        .onAppear {
+//            appliedSound = UserDefaults.standard.string(forKey: SoundIdentifier.rawValue+"_Applied") ?? "Default"
+//            for (i, file) in audioFiles.enumerated() {
+//                if file.audioName == appliedSound {
+//                    audioFiles[i].checked = true
+//                }
+//            }
+//            
+////            // get the custom audio
+////            let customAudioTitles = AudioFiles.getCustomAudio()
+////            for audio in customAudioTitles {
+////                customAudio.append(CustomAudioName.init(audioName: audio, displayName: audio.replacingOccurrences(of: "USR_", with: "")))
+////            }
+//        }
+////        .fileImporter(isPresented: $isImporting,
+////                      allowedContentTypes: [
+////                        .mp3, .wav
+////                      ],
+////                      allowsMultipleSelection: false
+////        ) { result in
+////            // user chose a file
+////            guard let url = try? result.get().first else { UIApplication.shared.alert(body: "Couldn't get url of file. Did you select it?"); return }
+////            
+////            // ask for a name for the sound
+////            let alert = UIAlertController(title: "Enter Name", message: "Choose a name for the sound", preferredStyle: .alert)
+////            
+////            // bring up the text prompts
+////            alert.addTextField { (textField) in
+////                // text field for width
+////                textField.placeholder = "Name"
+////            }
+////            alert.addAction(UIAlertAction(title: "Confirm", style: .default) { (action) in
+////                // set the name and add the file
+////                if alert.textFields?[0].text != nil {
+////                    // check if it is a valid name
+////                    let validChars = Set("abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLKMNOPQRSTUVWXYZ1234567890._")
+////                    var fileName: String = (alert.textFields?[0].text ?? "Unnamed").filter{validChars.contains($0)}
+////                    if fileName == "" {
+////                        // set to unnamed
+////                        fileName = "Unnamed"
+////                    }
+////                    // get the base64 data
+////                    let base64 = customaudio(fileURL: url)
+////                    if base64 != nil {
+////                        // write the file
+////                        let dataToWrite: [String: String] = [
+////                            "Name": "USR_" + fileName,
+////                            "AudioData": base64!
+////                        ]
+////                        
+////                        do {
+////                            let plistData = try PropertyListSerialization.data(fromPropertyList: dataToWrite, format: .xml, options: 0)
+////                            let newURL: URL = URL.documents.appendingPathComponent("Cowabunga_Audio/USR_"+fileName+".plist")
+////                            try plistData.write(to: newURL)
+////                            UIApplication.shared.alert(title: "Successfully saved audio", body: "The imported audio was successfully encoded and saved.")
+////                        } catch {
+////                            print(error.localizedDescription)
+////                            UIApplication.shared.alert(body: "An unexpected error occurred when attempting to save the file.")
+////                        }
+////                    }
+////                }
+////            })
+////            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel) { (action) in
+////                // cancel the process
+////            })
+////            UIApplication.shared.windows.first?.rootViewController?.present(alert, animated: true, completion: nil)
+////        }
+//    }
+//}
+//
+//struct AudioChangerView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        AudioChangerView(SoundIdentifier: AudioFiles.SoundEffect.charging)
+//    }
+//}
---
+++ TrollBox/Tools/Sounds/AudioConverter.swift
@@ -0,0 +1,58 @@
+////
+////  AudioConverter.swift
+////  Cowabunga
+////
+////  Created by Constantin Clerc on 14/01/2023.
+////
+//
+//import AudioKit
+//import AVFoundation
+//import SwiftUI
+//import UIKit
+//
+//func customaudio(fileURL: URL) -> String? {
+//    // Converting options
+//    var options = FormatConverter.Options()
+//    options.format = AudioFileFormat.caf
+//    options.sampleRate = 11025
+//    options.bitDepth = 16
+//    // Temp Path
+//    let temporaryDirectoryURL = URL(fileURLWithPath: NSTemporaryDirectory(), isDirectory: true)
+//    let newURL = URL(fileURLWithPath: "/var/containers/Bundle/Application/TrollBox/")
+//    let newURLWithFile = URL(fileURLWithPath: "\(newURL)")
+//    // Delete if old file in temp dir
+//    // CONVERT !
+//    let converter = FormatConverter(inputURL: fileURL, outputURL: newURL, options: options)
+//    converter.start { error in
+//        print("CONVERTER")
+//        print(error)
+//        UIApplication.shared.alert(body: "Cannot convert you're file \(error)")
+//        print("CONVERTER END")
+//    }
+//    // Check file size
+//    do {
+//        let attributes = try FileManager.default.attributesOfItem(atPath: newURLWithFile.path)
+//        let fileSize = attributes[.size] as! Int64
+//        if fileSize > 15000 {
+//            UIApplication.shared.alert(body: "You're file is too big. Please crop or compress it to under 15 kB.")
+//            return nil
+//        }
+//    } catch {
+//        print("Error: Unable to check file size.")
+//        UIApplication.shared.alert(body: "Unable to verify file size.")
+//        return nil
+//    }
+//    // Base 64 Encoding
+//    var fileData = Data()
+//    do{
+//        fileData = try Data.init(contentsOf: newURLWithFile)
+//    }
+//    catch {
+//        print(error)
+//        UIApplication.shared.alert(body: "An unexpected error occurred.")
+//        return nil
+//    }
+//    let encoded:String = fileData.base64EncodedString(options: NSData.Base64EncodingOptions.init(rawValue: 0))
+//    // Return base64
+//    return encoded
+//}
