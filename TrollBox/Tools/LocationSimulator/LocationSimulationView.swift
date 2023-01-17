5d502a35d01f4afc525466b3eccb5334ed8f47c1
c22dev <constclerc@gmail.com>
2023-01-18 00:05:52 +0100

    project bookmarks

--- TrollBox/Tools/LocationSimulator/LocationSimulationView.swift
+++ TrollBox/Tools/LocationSimulator/LocationSimulationView.swift
@@ -40,40 +40,40 @@ struct LocationSimulationView: View {
             }) {
                 Text("Apply")
             }
-            Button(action: {
-                let alert = UIAlertController(title: "Add to Bookmark", message: "Enter bookmark name", preferredStyle: .alert)
-                alert.addTextField { (textField) in
-                    textField.placeholder = "List Name"
-                }
-                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak alert] (_) in
-                    let textField = alert?.textFields![0]
-                    self.currentListName = textField?.text ?? ""
-                    let latitudeValue = Double(lat)
-                    let longitudeValue = Double(long)
-                    self.bookmarks[self.currentListName] = (latitudeValue!, longitudeValue!)
-                    UserDefaults.standard.set(self.bookmarks, forKey: "bookmarks")
-                }))
-                UIApplication.shared.windows.first?.rootViewController?.present(alert, animated: true, completion: nil)
-            }) {
-                Text("Add to Bookmark")
-            }
-            Button(action: {
-                            let alert = UIAlertController(title: "Bookmarks", message: "Choose a list", preferredStyle: .alert)
-                            for (name, _) in bookmarks {
-                                alert.addAction(UIAlertAction(title: name, style: .default, handler: { (_) in
-                                    self.currentListName = name
-                                    let (lat, long) = self.bookmarks[self.currentListName]!
-                                    self.lat = "\(lat)"
-                                    self.long = "\(long)"
-                                    self.locations = [.init(coordinate: .init(latitude: lat, longitude: long ))]
-                                    calculateDirections()
-                                    LocSimManager.startLocSim(location: .init(latitude: lat, longitude: long))
-                                }))
-                            }
-                            UIApplication.shared.windows.first?.rootViewController?.present(alert, animated: true, completion: nil)
-                        }) {
-                            Text("Bookmarks")
-                        }
+//            Button(action: {
+//                let alert = UIAlertController(title: "Add to Bookmark", message: "Enter bookmark name", preferredStyle: .alert)
+//                alert.addTextField { (textField) in
+//                    textField.placeholder = "List Name"
+//                }
+//                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak alert] (_) in
+//                    let textField = alert?.textFields![0]
+//                    self.currentListName = textField?.text ?? ""
+//                    let latitudeValue = Double(lat)
+//                    let longitudeValue = Double(long)
+//                    self.bookmarks[self.currentListName] = (latitudeValue!, longitudeValue!)
+//                    UserDefaults.standard.set(self.bookmarks, forKey: "bookmarks")
+//                }))
+//                UIApplication.shared.windows.first?.rootViewController?.present(alert, animated: true, completion: nil)
+//            }) {
+//                Text("Add to Bookmark")
+//            }
+//            Button(action: {
+//                            let alert = UIAlertController(title: "Bookmarks", message: "Choose a list", preferredStyle: .alert)
+//                            for (name, _) in bookmarks {
+//                                alert.addAction(UIAlertAction(title: name, style: .default, handler: { (_) in
+//                                    self.currentListName = name
+//                                    let (lat, long) = self.bookmarks[self.currentListName]!
+//                                    self.lat = "\(lat)"
+//                                    self.long = "\(long)"
+//                                    self.locations = [.init(coordinate: .init(latitude: lat, longitude: long ))]
+//                                    calculateDirections()
+//                                    LocSimManager.startLocSim(location: .init(latitude: lat, longitude: long))
+//                                }))
+//                            }
+//                            UIApplication.shared.windows.first?.rootViewController?.present(alert, animated: true, completion: nil)
+//                        }) {
+//                            Text("Bookmarks")
+//                        }
         }
         Button(action: {
             LocSimManager.stopLocSim()
@@ -85,14 +85,6 @@ struct LocationSimulationView: View {
             coordinateRegion: $region,
             informationVisibility: .default.union(.userLocation),
             interactionModes: [.all],
-            annotationItems: locations,
-            annotationContent: { location in
-                ViewMapAnnotation(coordinate: location.coordinate) {
-                    Color.accentColor
-                        .frame(width: 24, height: 24)
-                        .clipShape(Circle())
-                }
-            },
             overlays: directions?.routes.map { $0.polyline } ?? [],
             overlayContent: { overlay in
                 RendererMapOverlay(overlay: overlay) { (mapView, overlay) in
