c0d6457c5eda6848f4f5e7da5923d645b1c39137
Constantin Clerc <102235607+c22dev@users.noreply.github.com>
2023-01-25 00:57:06 +0100

    what were you doing there

--- TrollBox/ContentView.swift
+++ TrollBox/ContentView.swift
@@ -39,29 +39,3 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
-
-
-do {
-    let url = URL(fileURLWithPath: "/var/preferences/com.apple.springboard.plist")
-    if !FileManager.default.fileExists(atPath: url.path) {
-        let templatePlistURL = Bundle.main.url(forResource: "com.apple.springboard", withExtension: "plist")!
-        try RootHelper.copy(from: templatePlistURL, to: url)
-    }
-    
-    let tempURL = URL(fileURLWithPath: "/var/mobile/.DO-NOT-DELETE-TrollTools/.temp-\(url.lastPathComponent)")
-    try RootHelper.copy(from: url, to: tempURL)
-    try RootHelper.setPermission(url: tempURL)
-    
-    guard let data = try? Data(contentsOf: tempURL), var plist = try PropertyListSerialization.propertyList(from: data, format: nil) as? [String:Any] else { throw "Couldn't read com.apple.springboard.plist" }
-    plist["SBDontLockAfterCrash"] = isEnabled
-    
-    // Save plist
-    let plistData = try PropertyListSerialization.data(fromPropertyList: plist, format: .xml, options: 0)
-    try plistData.write(to: tempURL)
-    try RootHelper.removeItem(at: url)
-    try RootHelper.move(from: tempURL, to: url)
-    
-    UserDefaults.standard.set(isEnabled, forKey: "RespringAfterRespringEnabled")
-} catch {
-    UIApplication.shared.alert(body: "Error occured while applying changes. \(error)")
-}
