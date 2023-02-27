547bee2c4a2225b77cda978e5d3c1cc966a311b9
diyar2137237243 <diyarpc6@gmail.com>
2023-02-27 13:55:47 +0100

    changed bridging header

--- TrollBox/Libs/TrollBox-Bridging-Header.h
+++ TrollBox/Libs/TrollBox-Bridging-Header.h
@@ -1,7 +1,7 @@
 //
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
-
+#import "StatusManager.h"
 #import "vm_unaligned_copy_switch_race.h"
 #import "grant_full_disk_access.h"
 #import "TSUtil.h"
@@ -13,7 +13,6 @@
 #import "ttcpad.h"
 #import "stripttc.h"
 #import "helpers.h"
-#import "StatusManager.h"
 void invoke(SEL selector, id target, void *arg);
 void invokeInt(SEL selector, id target, long long arg);
 void invokeDouble(SEL selector, id target, double arg);
